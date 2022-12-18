7a516ece9f14318371a80d5cb8a15237059f7631
Dave H <d.a.halloran@gmail.com>
2022-12-17 19:14:02 -0500

    problems

--- portfolio_website/src/components/Home.js
+++ portfolio_website/src/components/Home.js
@@ -1,11 +1,53 @@
 import '../styles/home.css'
+import { useState, useEffect } from 'react'
 
-function Home() {
+
+const Home = () => {
+    const [textOne, setTextOne] = useState("")
+    const [textTwo, setTextTwo] = useState(null)
+    const [textThree, setTextThree] = useState(null)
+    let lineOneText = "Hi, I'm Dave."
+    let lineTwoText = "I have a Master's in Computer Science."
+    let lineThreeText = "I like to code websites, apps, & programs."
+
+
+    function runSetTimeout(currentChar, setText, interval) {
+        setTimeout = () => {
+            let updatedText = textOne + currentChar
+            setText(updatedText);
+        }, interval;
+    }
+
+    function iterateText(currentText, setText) {
+        for (let i = 0; i < currentText.length; i++) {
+            let currentChar = currentText[i]
+            let interval = 0
+            switch (currentText[i]) {
+                case ',':
+                    interval = 2000
+                case '.':
+                    interval = 3500
+                default:
+                    interval = 100000000
+            }
+            runSetTimeout(currentChar, setText, interval)
+        }
+    }
+
+    // useEffect(() => {
+    //     console.log('use effect')
+    //     console.log(textOne)
+    // }, [])
+
+
+
+    iterateText(lineOneText, setTextOne)
+    setTextOne("test")
     return (
         <div className="homeContainer">
-            <div className='line1'>Hi, I'm Dave.</div>
-            <div className='line2'>I have a Master's in Computer Science.</div>
-            <div className='line3'>I like to code.</div>
+            <div className='line1'>{textOne}</div>
+            <div className='line2'>{textTwo}</div>
+            <div className='line3'>{textThree}</div>
         </div>
     );
 }
